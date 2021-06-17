eb98d02f598e8c89ccfae8284f6e6a8ece89e89a
Pravesh <praveshdwivedi999@gmail.com>
2021-06-17 15:58:55 +0530

    modified server.js

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ require("./app/routes/user.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port http://localhost: ${PORT}.`);
 });
